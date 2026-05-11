7a97553aa9cc99d0c0933e4d25a583a5e90fcd38
youssefwilliams2004 <youssefwilliams04@gmail.com>
2026-05-11 15:53:00 +0100

    Adjusted app.py and base.html

---
+++ app.py
@@ -0,0 +1,38 @@
+from models.db import db
+from flask import Flask, render_template, request 
+from datetime import date
+
+
+#from routes.routes_patient import patient_bp 
+
+#from routes.routes_employee import employee_bp 
+
+#from routes.routes_medicine import medicine_bp 
+
+
+
+app=Flask(__name__) 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hospital.db'
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+#app.register_blueprint(patient_bp)
+#app.register_blueprint(employee_bp)
+#app.register_blueprint(medicine_bp)
+
+
+
+@app.route('/')
+def base():
+    return render_template('base.html')
+
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
+
+
